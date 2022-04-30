39b0ae5bc6d41a590049367ec4d2809e4d12d4e5
tbayryyev <mrtahyr@gmail.com>
2022-04-30 17:56:43 -0400

    added API route

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const appointmentRoutes = require('./appointments');
+const API = require('./API');
 
 const constructorMethod = (app) => {
 
   app.use('/appointments', appointmentRoutes);
-  app.use('/', function (req, res) {
-    res.sendFile(path.resolve('static/index.html'));
-  });
+  app.use('/', API);
   app.use('*', (req, res) => {
     res.sendStatus(404);
   });

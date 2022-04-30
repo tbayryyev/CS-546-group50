322d910567f1bb7228b40bc8766207f2b06608e9
tbayryyev <mrtahyr@gmail.com>
2022-04-30 19:27:44 -0400

    developing home page

--- routes/API.js
+++ routes/API.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const dotorData = require('../data/doctors');
+const Data = require('../data');
+const doctorData = Data.doctors;
+
+router
+    .route('')
+    .get(async (req, res) => {
+        res.render('pages/home_page');
+    });
+
+module.exports = router;
